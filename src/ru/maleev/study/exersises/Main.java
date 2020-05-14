e091927a87d6e2c3f2f79fcf6b5d91a68117a388
Mike Maleev <chip_01@bk.ru>
2020-05-15 00:18:01 +0300

    Добавлены комментарии, укорочены переменные в методах

--- src/ru/maleev/study/exersises/Main.java
+++ src/ru/maleev/study/exersises/Main.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class Main {               // программа реализует шифр Цезаря - сдвиг всех алфавитных символов строки на определенное число символов вправо
 
-    public static int key = 15; //ввести проверку целого и неотрицательного key
-
+    public static int key = 15;   //ввести проверку целого и неотрицательного key
     public static void main(String[] args) {
 
+        // Формирование алфавитов
         char [] arr_abc = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
         List<Character> alphabet_en = new ArrayList<Character>();
         for (char ch: arr_abc) alphabet_en.add(ch);
@@ -34,52 +34,52 @@ public class Main {
         System.out.println(encriptString);
 //        String decriptString = decript(s, alphabet_en, alphabet_EN, alphabet_ru, alphabet_RU);
 //        System.out.println(decriptString);
-        System.out.println('\u0950');
+        System.out.println('\u0950'); // Знак Ом
     }
 
-    public static String encript (String s, List en, List EN, List ru, List RU) {
+    public static String encript (String s, List en, List EN, List ru, List RU) {  // метод шифрования
         char [] charArray = s.toCharArray();
         StringBuilder resultString = new StringBuilder();
-        for (char currentChar: charArray) {
-            if (en.contains(currentChar)) {
-                currentChar = (char) en.get((en.indexOf(currentChar) + key) % en.size());
-            } else if (EN.contains(currentChar)) {
-                currentChar = (char) EN.get((EN.indexOf(currentChar) + key) % EN.size());
-            } else if (ru.contains(currentChar)) {
-                currentChar = (char) ru.get((ru.indexOf(currentChar) + key) % ru.size());
-            } else if (RU.contains(currentChar)) {
-                currentChar = (char) RU.get((RU.indexOf(currentChar) + key) % RU.size());
+        for (char ch: charArray) {
+            if (en.contains(ch)) {
+                ch = (char) en.get((en.indexOf(ch) + key) % en.size());
+            } else if (EN.contains(ch)) {
+                ch = (char) EN.get((EN.indexOf(ch) + key) % EN.size());
+            } else if (ru.contains(ch)) {
+                ch = (char) ru.get((ru.indexOf(ch) + key) % ru.size());
+            } else if (RU.contains(ch)) {
+                ch = (char) RU.get((RU.indexOf(ch) + key) % RU.size());
             }
-            resultString.append(currentChar);
+            resultString.append(ch);
         }
         return String.valueOf(resultString);
     }
 
-    public static String decript (String s, List en, List EN, List ru, List RU) {
+    public static String decript (String s, List en, List EN, List ru, List RU) {  // метод расшифровщика шифра
         char [] charArray = s.toCharArray();
         StringBuilder resultString = new StringBuilder();
-        for (char currentChar: charArray) {
-            if (en.contains(currentChar)) {
-                if ((en.indexOf(currentChar) - key) < 0 ) {
-                    currentChar = (char) en.get(en.size() - (key - en.indexOf(currentChar)) % en.size());
-                } else currentChar = (char) en.get(en.indexOf(currentChar) - key);
+        for (char ch: charArray) {
+            if (en.contains(ch)) {
+                if ((en.indexOf(ch) - key) < 0 ) {
+                    ch = (char) en.get(en.size() - (key - en.indexOf(ch)) % en.size());
+                } else ch = (char) en.get(en.indexOf(ch) - key);
 
-            } else if (EN.contains(currentChar)) {
-                if ((EN.indexOf(currentChar) - key) < 0 ) {
-                    currentChar = (char) EN.get(EN.size() - (key - EN.indexOf(currentChar)) % EN.size());
-                } else currentChar = (char) EN.get(EN.indexOf(currentChar) - key);
+            } else if (EN.contains(ch)) {
+                if ((EN.indexOf(ch) - key) < 0 ) {
+                    ch = (char) EN.get(EN.size() - (key - EN.indexOf(ch)) % EN.size());
+                } else ch = (char) EN.get(EN.indexOf(ch) - key);
 
-            } else if (ru.contains(currentChar)) {
-                if ((ru.indexOf(currentChar) - key) < 0 ) {
-                    currentChar = (char) ru.get(ru.size() - (key - ru.indexOf(currentChar)) % ru.size());
-                } else currentChar = (char) ru.get(ru.indexOf(currentChar) - key);
+            } else if (ru.contains(ch)) {
+                if ((ru.indexOf(ch) - key) < 0 ) {
+                    ch = (char) ru.get(ru.size() - (key - ru.indexOf(ch)) % ru.size());
+                } else ch = (char) ru.get(ru.indexOf(ch) - key);
 
-            } else if (RU.contains(currentChar)) {
-                if ((RU.indexOf(currentChar) - key) < 0 ) {
-                    currentChar = (char) RU.get(RU.size() - (key - RU.indexOf(currentChar)) % RU.size());
-                } else currentChar = (char) RU.get(RU.indexOf(currentChar) - key);
+            } else if (RU.contains(ch)) {
+                if ((RU.indexOf(ch) - key) < 0 ) {
+                    ch = (char) RU.get(RU.size() - (key - RU.indexOf(ch)) % RU.size());
+                } else ch = (char) RU.get(RU.indexOf(ch) - key);
             }
-            resultString.append(currentChar);
+            resultString.append(ch);
         }
         return String.valueOf(resultString);
     }
